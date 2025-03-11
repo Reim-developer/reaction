2adc6382373c1d4b2a4bd51a57ef689f5a24cc1b
Reim-developer <contact.kaxtr@gmail.com>
2025-03-11 23:07:48 +0700

    New params *windows for setup_ui_new

--- src/components.c
+++ src/components.c
@@ -3,8 +3,8 @@
 #include "button.h"
 #include "label.h"
 
-void setup_ui_new(GtkWidget *windows_grid_t) {
-    button_t *open_usb_btn_t = open_usb_button_new();
+void setup_ui_new(GtkWidget *windows_grid_t, GtkWidget *windows) {
+    button_t *open_usb_btn_t = open_usb_button_new(windows);
     gtk_grid_attach(GTK_GRID(windows_grid_t),
         open_usb_btn_t->button,
         open_usb_btn_t->grid_column,
@@ -22,6 +22,14 @@ void setup_ui_new(GtkWidget *windows_grid_t) {
         label_description_top->grid_height
     );
 
+    dropbox_t *device_list_t = dropbox_set_new();
+    gtk_grid_attach(GTK_GRID(windows_grid_t),
+    device_list_t->dropbox, device_list_t->grid_column,
+    device_list_t->grid_row, device_list_t->grid_width,
+    device_list_t->grid_height
+    );
+
     g_free(open_usb_btn_t);
     g_free(label_description_top);
+    g_free(device_list_t);
 }

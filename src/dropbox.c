a6f447a03e251efda3e557185f5df983b4dbf812
Reim-developer <contact.kaxtr@gmail.com>
2025-03-11 23:08:20 +0700

    Removed TODO/Test value in GListModel

--- src/dropbox.c
+++ src/dropbox.c
@@ -3,10 +3,23 @@
 dropbox_t *dropbox_set_new(void) {
     dropbox_t *dropbox_device_t = g_new(dropbox_t, 1);
     GListModel *device_list = G_LIST_MODEL(gtk_string_list_new((const char *[]) {
-        "Option 1", "Option 2"
+        "No USB found"
     }));
     dropbox_device_t->device_model_list = device_list;
     dropbox_device_t->dropbox = gtk_drop_down_new(dropbox_device_t->device_model_list, NULL);
+
+    dropbox_device_t->grid_column = 2;
+    dropbox_device_t->grid_row = 4;
+
+    dropbox_device_t->grid_height = 3;
+    dropbox_device_t->grid_width = 3;
+
+    dropbox_device_t->width = 100;
+    dropbox_device_t->height = 10;
+
+    gtk_widget_set_size_request(GTK_WIDGET(dropbox_device_t->dropbox),
+        dropbox_device_t->width, dropbox_device_t->height
+    );
 
     return dropbox_device_t;
 }

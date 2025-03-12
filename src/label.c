8cb4f7546aa273f0fb7ab9573b8964906523ae73
Reim-developer <contact.kaxtr@gmail.com>
2025-03-12 12:14:37 +0700

    Fixed UI layout

--- src/label.c
+++ src/label.c
@@ -5,10 +5,10 @@ label_t *label_description_new() {
     label_description_top->label = gtk_label_new(NULL);
     label_description_top->width = 10;
     label_description_top->height = 20;
-    label_description_top->grid_column = 2;
+    label_description_top->grid_column = 4;
     label_description_top->grid_row = 2;
-    label_description_top->grid_width = 2;
-    label_description_top->grid_height = 2;
+    label_description_top->grid_width = 1;
+    label_description_top->grid_height = 1;
 
     gtk_label_set_markup(GTK_LABEL(label_description_top->label), "<big> Drive Properties </big>");
 

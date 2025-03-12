2ed5dd029c05fb500c2210def62f49c59db3e979
Reim-developer <contact.kaxtr@gmail.com>
2025-03-12 23:01:38 +0700

    Fixed UI layout value: width = 10 -> 20 height = 20 -> 1 grid_column = 6 -> 9 grid_width = 3 -> 5

--- src/button.c
+++ src/button.c
@@ -5,13 +5,13 @@ button_t *open_usb_button_new(GtkWidget *windows) {
     button_t *open_usb_button = g_new(button_t, 1);
     open_usb_button->button = gtk_button_new_with_label("Choose USB...");
 
-    open_usb_button->width = 10;
-    open_usb_button->height = 20;
+    open_usb_button->width = 20;
+    open_usb_button->height = 1;
 
-    open_usb_button->grid_column = 6;
+    open_usb_button->grid_column = 9;
     open_usb_button->grid_row = 5;
 
-    open_usb_button->grid_width = 3;
+    open_usb_button->grid_width = 5;
     open_usb_button->grid_height = 1;
 
     open_usb_button->device_list_t = NULL;

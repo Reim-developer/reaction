9a1c0d56724b278de99a66a0745915b8e189638a
Reim-developer <contact.kaxtr@gmail.com>
2025-03-13 02:27:46 +0700

    Fixed types of field in struct button_t int width -> unsigned int width int height -> unsigned int height int grid_column -> unsigned int grid_column int grid_row -> unsigned int grid_row int grid_width -> unsigned int grid_width int grid_height -> unsigned grid_height
    Renamed two void function:
    open_usb_button_new to -> open_usb_button_new_widget
    
    Added new void function
    open_file_button_new_widget
    
    Added two static void callback:
    on_open_usb_button_clicked
    on_open_file_dialog

--- src/button.h
+++ src/button.h
@@ -1,25 +1,37 @@
+// Define:
+// Button widget.
+// Button callback event.
+// Dialog callback event. (Open when clicked a button widget).
 #ifndef BUTTON_H
 #define BUTTON_H
 #include "dropbox.h"
 #include <gtk/gtk.h>
 
+// Button widget struct.
 typedef struct {
     GtkWidget *button;
-    int width;
-    int height;
-    int grid_column;
-    int grid_row;
-    int grid_width;
-    int grid_height;
     dropbox_t *device_list_t;
-
+    unsigned int width;
+    unsigned int height;
+    unsigned int grid_column;
+    unsigned int grid_row;
+    unsigned int grid_width;
+    unsigned int grid_height;
 } button_t;
 
-/*
-* Defined OPEN_USB_BUTTON and set this even, when click to Open USB button
-*/
-button_t *open_usb_button_new(GtkWidget *windows);
-static void open_usb_button_clicked(GtkButton *open_usb_button, gpointer data);
+// Create new: open_usb_button widget.
+button_t *open_usb_button_new_widget(GtkWidget *windows);
+// Create new: open_file_button widget.
+button_t *open_file_button_new_widget(GtkWidget *windows);
+
+// Callback event for handling: open_usb_button widget.
+static void on_open_usb_button_clicked(GtkButton *open_usb_button, gpointer data);
+// Callback event for handling: open_usb_dialog dialog.
 static void on_open_usb_dialog(GtkDialog *dialog, int response_id, gpointer data);
 
-#endif
+// Callback event for handling: open_file_button widget.
+static void on_open_file_button_clicked(GtkButton *open_file_button, gpointer data);
+// Callback event for handling: open_file_dialog dialog.
+static void on_open_file_dialog(GtkDialog *dialog, int response_id, gpointer data);
+
+#endif // BUTTON_H

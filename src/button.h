9de8ca4a2488f1b1262bf46e92fcd62d422601cc
Reim-developer <contact.kaxtr@gmail.com>
2025-03-14 18:56:40 +0700

    Change log: - Removed 'unsigned' type in fields: * width * height * grid_column * grid_row * grid_width * grid_height
    - Removed static function defined

--- src/button.h
+++ src/button.h
@@ -11,27 +11,17 @@
 typedef struct {
     GtkWidget *button;
     dropbox_t *dropbox_data_t;
-    unsigned int width;
-    unsigned int height;
-    unsigned int grid_column;
-    unsigned int grid_row;
-    unsigned int grid_width;
-    unsigned int grid_height;
+    int width;
+    int height;
+    int grid_column;
+    int grid_row;
+    int grid_width;
+    int grid_height;
 } button_t;
 
 // Create new: open_usb_button widget.
 button_t *open_usb_button_new_widget(GtkWidget *windows);
 // Create new: open_file_button widget.
 button_t *open_file_button_new_widget(GtkWidget *windows);
-
-// Callback event for handling: open_usb_button widget.
-static void on_open_usb_button_clicked(GtkButton *open_usb_button, gpointer data);
-// Callback event for handling: open_usb_dialog dialog.
-static void on_open_usb_dialog(GtkDialog *dialog, int response_id, gpointer data);
-
-// Callback event for handling: open_file_button widget.
-static void on_open_file_button_clicked(GtkButton *open_file_button, gpointer data);
-// Callback event for handling: open_file_dialog dialog.
-static void on_open_file_dialog(GtkDialog *dialog, int response_id, gpointer data);
 
 #endif // BUTTON_H

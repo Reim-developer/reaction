551354f989e16eec0e1db44eb3a4a0101390a2e1
Reim-developer <contact.kaxtr@gmail.com>
2025-03-14 23:39:14 +0700

    Changelog: + Define button 'start_button' to UI

--- src/components.c
+++ src/components.c
@@ -2,6 +2,7 @@
 #include "dropbox.h"
 #include "button.h"
 #include "label.h"
+#include "panel.h"
 
 void setup_ui_new(GtkWidget *windows_grid_t, GtkWidget *windows) {
     button_t *open_usb_btn_t = open_usb_button_new_widget(windows);
@@ -18,6 +19,13 @@ void setup_ui_new(GtkWidget *windows_grid_t, GtkWidget *windows) {
         open_file_button_t->button, open_file_button_t->grid_column,
         open_file_button_t->grid_row, open_file_button_t->grid_width,
         open_file_button_t->grid_height
+    );
+
+    button_t *start_button_t = start_button_new_widget(windows);
+    gtk_grid_attach(GTK_GRID(windows_grid_t),
+        start_button_t->button, start_button_t->grid_column,
+        start_button_t->grid_row, start_button_t->grid_width,
+        start_button_t->grid_height
     );
 
     label_t *label_description_top = label_description_new();
@@ -40,6 +48,13 @@ void setup_ui_new(GtkWidget *windows_grid_t, GtkWidget *windows) {
     gtk_grid_attach(GTK_GRID(windows_grid_t), 
         file_list_t->dropbox, file_list_t->grid_column,
         file_list_t->grid_row, file_list_t->width, file_list_t->height
+    );
+
+    panel_t *text_result_view_t = result_panel_widget_new();
+    gtk_grid_attach(GTK_GRID(windows_grid_t),
+        text_result_view_t->panel_area_t, text_result_view_t->grid_column,
+        text_result_view_t->grid_row, text_result_view_t->grid_width,
+        text_result_view_t->grid_height
     );
 
     open_usb_btn_t->dropbox_data_t = device_list_t;

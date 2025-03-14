17d84726ccf3f9d97362fabb5761603606d35d3c
Reim-developer <contact.kaxtr@gmail.com>
2025-03-14 17:19:58 +0700

    Change log:
    - Fixed change in header file button.h
    In field device_list_t to -> dropbox_data_t
    
    - Added data in: 'g_signal_connect' with 'open_file_button_t
    
    -  Fixed all changed button.h header file:
    device_list_t -> dropbox_data_t
    
    - Removed printf function. Use g_warning instead
    
    - Added event callback in open_file_button_clicked callback function
    
    - Removed TODO comment

--- src/button.c
+++ src/button.c
@@ -14,7 +14,7 @@ button_t *open_usb_button_new_widget(GtkWidget *windows) {
     open_usb_button_t->grid_width = 5;
     open_usb_button_t->grid_height = 1;
 
-    open_usb_button_t->device_list_t = NULL;
+    open_usb_button_t->dropbox_data_t = NULL;
 
     gtk_widget_set_size_request(open_usb_button_t->button, open_usb_button_t->width, open_usb_button_t->height);
     g_signal_connect(open_usb_button_t->button, "clicked", G_CALLBACK(on_open_usb_button_clicked), open_usb_button_t);
@@ -34,8 +34,10 @@ button_t *open_file_button_new_widget(GtkWidget *windows) {
     open_file_button_t->grid_width = 5;
     open_file_button_t->grid_height = 1;
 
+    open_file_button_t->dropbox_data_t = NULL;
+
     gtk_widget_set_size_request(open_file_button_t->button, open_file_button_t->width, open_file_button_t->height);
-    g_signal_connect(open_file_button_t->button, "clicked", G_CALLBACK(on_open_file_button_clicked), NULL);
+    g_signal_connect(open_file_button_t->button, "clicked", G_CALLBACK(on_open_file_button_clicked), open_file_button_t);
 
     return open_file_button_t;
 }
@@ -43,10 +45,10 @@ button_t *open_file_button_new_widget(GtkWidget *windows) {
 static void on_open_usb_dialog(GtkDialog *dialog, int response_id, gpointer data) {
     if(response_id == GTK_RESPONSE_ACCEPT) {
        button_t *open_usb_button_t = (button_t *) data;
-       dropbox_t *device_list_t = open_usb_button_t->device_list_t;
+       dropbox_t *dropbox_data_t = open_usb_button_t->dropbox_data_t;
 
-       if (device_list_t == NULL) {
-            g_warning("device_list_t is NULL. Stop program");
+       if (dropbox_data_t == NULL) {
+            g_warning("dropbox_data_t is NULL. Stop program");
             return;
        }
 
@@ -55,7 +57,7 @@ static void on_open_usb_dialog(GtkDialog *dialog, int response_id, gpointer data
        char *device_directory = g_file_t ? g_file_get_path(g_file_t) : NULL;
 
        if (device_directory == NULL) {
-            printf("device_directory is NULL. Stop program");
+            g_warning("device_directory is NULL. Stop program");
             return;
        }
        char *device_name = g_path_get_basename(device_directory);
@@ -64,8 +66,8 @@ static void on_open_usb_dialog(GtkDialog *dialog, int response_id, gpointer data
             device_name, NULL
        }));
 
-       device_list_t->device_model_list = device_list_model_t;
-       gtk_drop_down_set_model(GTK_DROP_DOWN(device_list_t->dropbox), device_list_t->device_model_list);
+       dropbox_data_t->device_model_list = device_list_model_t;
+       gtk_drop_down_set_model(GTK_DROP_DOWN(dropbox_data_t->dropbox), dropbox_data_t->device_model_list);
 
        g_free(device_directory);
        g_free(device_name);
@@ -76,7 +78,7 @@ static void on_open_usb_dialog(GtkDialog *dialog, int response_id, gpointer data
 
 static void on_open_usb_button_clicked(GtkButton *button, gpointer data) {
     button_t *open_usb_button_t = (button_t *) data;
-    dropbox_t *device_list_t = open_usb_button_t->device_list_t;
+    dropbox_t *dropbox_data_t = open_usb_button_t->dropbox_data_t;
 
     GtkWindow *main_windows = GTK_WINDOW(gtk_widget_get_ancestor(GTK_WIDGET(button), GTK_TYPE_WINDOW));
 
@@ -95,6 +97,9 @@ static void on_open_usb_button_clicked(GtkButton *button, gpointer data) {
 }
 
 static void on_open_file_button_clicked(GtkButton *button, gpointer data) {
+    button_t *open_file_button_t = (button_t *) data;
+    dropbox_t *iso_list_file_t = open_file_button_t->dropbox_data_t;
+
     GtkWindow *main_windows = GTK_WINDOW(gtk_widget_get_ancestor(GTK_WIDGET(button), GTK_TYPE_WINDOW));
 
     GtkFileChooserDialog *choose_file_dialog = g_object_new(
@@ -113,13 +118,40 @@ static void on_open_file_button_clicked(GtkButton *button, gpointer data) {
     gtk_file_filter_set_name(filter_file_t, "Boot File (*.iso, *.img)");
     gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(choose_file_dialog), filter_file_t);
 
-    g_signal_connect(choose_file_dialog, "response", G_CALLBACK(on_open_file_dialog), NULL);
+    g_signal_connect(choose_file_dialog, "response", G_CALLBACK(on_open_file_dialog), open_file_button_t);
     gtk_window_present(GTK_WINDOW(choose_file_dialog));
 }
 
 static void on_open_file_dialog(GtkDialog *dialog, int response_id, gpointer data) {
     if(response_id == GTK_RESPONSE_ACCEPT) {
-        // TODO: Setup event handling for this callback
+        button_t *open_file_button_t = (button_t *) data;
+        dropbox_t *boot_file_list_t =  open_file_button_t->dropbox_data_t;
+
+        if (boot_file_list_t == NULL) {
+            g_warning("BOOT_FILE_LIST_T is NULL. Stop program");
+            return;
+        }
+
+        GtkFileChooser *file_chooser_t = GTK_FILE_CHOOSER(dialog);
+        GFile *g_file_t = gtk_file_chooser_get_file(file_chooser_t);
+        char *boot_file_path = g_file_t ? g_file_get_path(g_file_t) : NULL;
+
+        if (boot_file_path == NULL) {
+            g_warning("BOOT_FILE_PATH is NULL. Stop program");
+            return;
+        }
+        char *boot_file_name = g_path_get_basename(boot_file_path);
+
+        GListModel *boot_file_model_t = G_LIST_MODEL(gtk_string_list_new((const char *[]) {
+            boot_file_name, NULL
+        }));
+
+        boot_file_list_t->device_model_list = boot_file_model_t;
+        gtk_drop_down_set_model(GTK_DROP_DOWN(boot_file_list_t->dropbox), boot_file_list_t->device_model_list);
+
+        g_free(boot_file_path);
+        g_free(boot_file_name);
+        g_object_unref(g_file_t);
     }
     gtk_window_close(GTK_WINDOW(dialog));
 }

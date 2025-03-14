0fa7e4edd34158c7727de23e91da1d942a203d41
Reim-developer <contact.kaxtr@gmail.com>
2025-03-14 19:02:59 +0700

    Change log: - Removed 'unsigned' type in fields: * app_width * app_height

--- src/main.h
+++ src/main.h
@@ -5,8 +5,8 @@
 typedef struct {
     char *app_name;
     char *app_version;
-    unsigned  app_width;
-    unsigned int app_height;
+    int app_width;
+    int app_height;
 } app_property_t;
 
 static void on_app_open(GtkApplication *g_app, gpointer data);

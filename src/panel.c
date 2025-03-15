e90041cb1c54891642835d9b2a62b2be550d686c
Reim-developer <contact.kaxtr@gmail.com>
2025-03-15 23:11:07 +0700

    Changelog: + Define new default text in panel text view + Defined int check result function.

--- src/panel.c
+++ src/panel.c
@@ -4,12 +4,13 @@ panel_t *result_panel_widget_new(void) {
     panel_t *scroll_widget_t = g_new(panel_t, 1);
     scroll_widget_t->panel_area_t = gtk_scrolled_window_new();
     GtkWidget *text_view_t = gtk_text_view_new();
+
     GtkTextBuffer *text_buffer_t = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view_t));
 
     gtk_widget_set_hexpand(scroll_widget_t->panel_area_t, TRUE);
     gtk_widget_set_vexpand(scroll_widget_t->panel_area_t, TRUE);
     gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scroll_widget_t->panel_area_t), text_view_t);
-    gtk_text_buffer_set_text(text_buffer_t, "- Nothing to show..\n", -1);
+    gtk_text_buffer_set_text(text_buffer_t, DEFAULT_TEXT_CONTENT, -1);
     gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view_t), FALSE);
 
     scroll_widget_t->width = 20;
@@ -49,4 +50,23 @@ void result_text_clear_new(panel_t *paneL_text_view_t) {
         GTK_TEXT_VIEW(text_view_t)
     );
     gtk_text_buffer_set_text(text_buffer_t, "", -1);
+}
+
+int is_default_text_view(panel_t *panel_text_view_t, const char *match) {
+    GtkWidget *text_view_t = gtk_scrolled_window_get_child(
+        GTK_SCROLLED_WINDOW(panel_text_view_t->panel_area_t)
+    );
+    GtkTextBuffer *text_buffer_t = gtk_text_view_get_buffer(
+        GTK_TEXT_VIEW(text_view_t)
+    );
+
+    GtkTextIter start_t, end_t;
+    gtk_text_buffer_get_start_iter(text_buffer_t, &start_t);
+    gtk_text_buffer_get_end_iter(text_buffer_t, &end_t);
+    gchar *current_text_t = gtk_text_buffer_get_text(text_buffer_t, &end_t, &start_t, FALSE);
+
+    int text_view_status = (strcmp(current_text_t, match) == 0) ? IS_DEFAULT_TEXT_VIEW : IS_NOT_DEFAULT_TEXT_VIEW;
+    g_free(current_text_t);
+
+    return text_view_status;
 }

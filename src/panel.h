0ada09f32daa9c742ebb09588fab5bbcbd6571aa
Reim-developer <contact.kaxtr@gmail.com>
2025-03-15 23:13:13 +0700

    Changelog: + Added new macro: * IS_DEFAULT_TEXT_VIEW. Expands to 1 * DEFAULT_TEXT_CONTENT. Expands to "Nothing to show..\n" * IS_NOT_DEFAULT_TEXT_VIEW. Expands to 0
    * Define new check result function with name:
    + is_default_text_view

--- src/panel.h
+++ src/panel.h
@@ -1,7 +1,9 @@
 #ifndef PANEL_H
 #define PANEL_H
+#define IS_DEFAULT_TEXT_VIEW 1
+#define DEFAULT_TEXT_CONTENT "Nothing to show..\n"
+#define IS_NOT_DEFAULT_TEXT_VIEW 0
 #include <gtk/gtk.h>
-
 
 typedef struct {
     GtkWidget *panel_area_t;
@@ -19,5 +21,7 @@ panel_t *result_panel_widget_new(void);
 void result_text_append_new(panel_t *panel_text_t, const char *content);
 // Clear text in text_view_panel widget.
 void result_text_clear_new(panel_t *panel_text_t);
+// Check text in text_view_panel widget.
+int is_default_text_view(panel_t *panel_text_t, const char *match);
 
 #endif // PANEL_H 

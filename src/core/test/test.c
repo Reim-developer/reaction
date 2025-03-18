efd2b9355961670b760defe10c99fc77c5bd35bc
Reim-developer <contact.kaxtr@gmail.com>
2025-03-18 14:35:24 +0700

    Added test case with error status code

--- src/core/test/test.c
+++ src/core/test/test.c
@@ -2,6 +2,9 @@
 
 int main() {
     const char *device = "/dev/sdb";
+    const char *wrong_device = "/dev/iswrongdevice";
     // format_device(device);
-    create_mbr_partition(device);
+    RESULT_STATUS status = create_mbr_partition(wrong_device);
+
+    printf("%d\n", status); // Status 1: Could not open device.
 }

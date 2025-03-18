86f5a95f7fa4d8e29dffda7520df2b4050917631
Reim-developer <contact.kaxtr@gmail.com>
2025-03-19 00:04:31 +0700

    Changelog: + Fix typo on header guard: FORMAT_H -> CORE_H + Added new enum field: * FILE_SYNC_FAILED = 6, * MOUNT_POINT_FOUND = 7, * MOUNT_POINT_NOT_FOUND = 8, * GET_MOUNT_ERROR = 9, * UNMOUNT_DEVICE_SUCCESS = 10, * UNMOUNT_DEVICE_FAILED =  11
    + Define 'unmount_device' function.

--- src/core/test/test.c
+++ src/core/test/test.c
@@ -1,10 +1,31 @@
 #include "core.h"
 
+/*
+* Main function for testing USB operations
+* 
+* Test cases are defined below. Uncomment the function
+* calls to execute specific tests.
+*/
 int main() {
     const char *device = "/dev/sdb";
-    const char *wrong_device = "/dev/iswrongdevice";
-    // format_device(device);
-    RESULT_STATUS status = create_mbr_partition(wrong_device);
+    
+   /*
+    * Case 1: Format USB device:
+    * Expected Result: The device should be formatted successfully.
+    */
+   // format_device(device);
 
-    printf("%d\n", status); // Status 1: Could not open device.
+  /*
+   * Case 2: Create MBR partition on the USB Device:
+   * Expected Result: The partition table should be created successfully.
+   */
+  // create_mbr_partition(device);
+  
+  /* 
+   * Case 3: Unmount the USB device
+   * Expected Status Code Result: 
+   * [+] 11 => Unmount failed. Try again with root operation
+   * [+] 10 => Unmount successfully.
+   */
+  // unmount_device(device); 
 }

c61de12b8b543ef45b1e292ac4bf70d6896aad7c
Reim-developer <contact.kaxtr@gmail.com>
2025-03-18 14:37:07 +0700

    Changelog: + Define 'RESULT_STATUS' enum + Removed void return type. Use 'RESULT_STATUS' enum instead.

--- src/core/core.h
+++ src/core/core.h
@@ -6,7 +6,17 @@
 #include <string.h>
 #include <parted/parted.h>
 
-void format_device(const char *device);
-void create_mbr_partition(const char *device);
+typedef enum {
+    SUCCESS = 0,
+    DEVICE_NOT_FOUND = 1,
+    ADD_PARTITION_FAILED = 2,
+    COMMIT_PARTITION_FAILED = 3,
+    WRITE_FAILED = 4,
+    OPEN_DEVICE_FAILED = 5,
+    FILE_SYNC_FAILED = 6
+} RESULT_STATUS;
+
+RESULT_STATUS format_device(const char *device);
+RESULT_STATUS create_mbr_partition(const char *device);
 
 #endif

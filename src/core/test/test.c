7dfe93a9f2d9a1da7e153e2c297f26accf68f6d8
Reim-developer <contact.kaxtr@gmail.com>
2025-03-20 23:56:17 +0700

    Test: Pass case 4: + Successfully to create FAT32 file system

--- src/core/test/test.c
+++ src/core/test/test.c
@@ -27,5 +27,13 @@ int main() {
    * [+] 11 => Unmount failed. Try again with root operation
    * [+] 10 => Unmount successfully.
    */
-  // unmount_device(device); 
+  // unmount_device(device);
+  
+ /*
+  * Case 4: Create FAT32 Filesystem
+  * Expected Status Code Result:
+  * [+] 0 => Success
+  * Else is error. Checkout in core.c
+  */
+  // create_fat32_file_system("/dev/sdb");
 }

2dea7754ea2238b9551043c4f3cb3b0fcab39963
Reim-developer <contact.kaxtr@gmail.com>
2025-03-20 23:54:42 +0700

    Removed error enum and migration this to common.h

--- src/core/core.h
+++ src/core/core.h
@@ -7,25 +7,12 @@
 #include <mntent.h>
 #include <sys/mount.h>
 #include <errno.h>
-
-typedef enum {
-    SUCCESS = 0,
-    DEVICE_NOT_FOUND = 1,
-    ADD_PARTITION_FAILED = 2,
-    COMMIT_PARTITION_FAILED = 3,
-    WRITE_FAILED = 4,
-    OPEN_DEVICE_FAILED = 5,
-    FILE_SYNC_FAILED = 6,
-    MOUNT_POINT_FOUND = 7,
-    MOUNT_POINT_NOT_FOUND = 8,
-    GET_MOUNT_ERROR = 9,
-    UNMOUNT_DEVICE_SUCCESS = 10,
-    UNMOUNT_DEVICE_FAILED =  11,
-    NO_PERMIT_OPERATION = 12
-} RESULT_STATUS;
+#include "fat32.h"
+#include "common.h"
 
 RESULT_STATUS unmount_device(const char *device);
 RESULT_STATUS format_device(const char *device);
 RESULT_STATUS create_mbr_partition(const char *device);
+RESULT_STATUS create_fat32_file_system(const char *device);
 
 #endif // CORE_H

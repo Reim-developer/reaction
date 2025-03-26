862c460e5c40a268a5cdbc238d423fe8d78cc94b
Reim-developer <contact.kaxtr@gmail.com>
2025-03-27 03:01:41 +0700

    Implement Geometry::moveCenter function to center a QWidget on the primary screen

---
+++ src/utils/utils.cc
@@ -0,0 +1,22 @@
+#include "utils.hpp"
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QApplication>
+#include <QtGui/QScreen>
+
+using namespace Reaction::Utils;
+
+void Geometry::moveCenter(QWidget *window) {
+    if(!window)
+    return;
+
+    QScreen *screen = QApplication::primaryScreen();
+    if(!screen)
+        return;
+
+    QRect screenGeometry = screen->geometry();
+
+    int x_point = (screenGeometry.width() - window->width() ) / 2;
+    int y_point = (screenGeometry.height() - window->height()) / 2;
+
+    window->move(x_point, y_point);
+}

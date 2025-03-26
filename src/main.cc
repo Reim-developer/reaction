f76a210902f74fd7388004ace272421d53edadcb
Reim-developer <contact.kaxtr@gmail.com>
2025-03-27 03:01:27 +0700

    Add using directive for Reaction namespace in main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,8 @@
 #include <QtWidgets/QApplication>
 #include "mainWindow.hpp"
 
+using namespace Reaction;
+
 int main(int argc, char *argv[]) {
     QApplication application(argc, argv);
     MainWindow window;

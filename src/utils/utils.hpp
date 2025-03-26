b2cf76526dae944ca8dc131b2d4b3bcc4a59b12c
Reim-developer <contact.kaxtr@gmail.com>
2025-03-27 03:01:46 +0700

    Refactor Geometry class to move moveCenter method declaration and update namespace to Reaction::Utils

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -1,27 +1,11 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
-#include <QtWidgets/QMainWindow>
-#include <QtWidgets/QApplication>
-#include <QtGui/QScreen>
+#include <QtWidgets/QWidget>
 
-namespace ReactionUtils {
+namespace Reaction::Utils {
     class Geometry {
         public:
-            static void moveCenter(QWidget *window) {
-                if(!window)
-                    return;
-                
-                QScreen *screen = QApplication::primaryScreen();
-                if(!screen)
-                    return;
-                
-                QRect screenGeometry = screen->geometry();
-
-                int x_point = (screenGeometry.width() - window->width() ) / 2;
-                int y_point = (screenGeometry.height() - window->height()) / 2;
-
-                window->move(x_point, y_point);
-        }
+            void moveCenter(QWidget *window);
     };
 }
 

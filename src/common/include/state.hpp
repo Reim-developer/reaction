7b6b0d724fcab975f4da9688786c119edd0de745
Reim-developer <contact.kaxtr@gmail.com>
2025-03-29 04:58:21 +0700

    Add State class to manage ISO file path

---
+++ src/common/include/state.hpp
@@ -0,0 +1,12 @@
+#ifndef STATE_HPP
+#define STATE_HPP
+#include <QtCore/QString>
+
+namespace Reaction::Common {
+    class State {
+        public:
+            QString isoFilePath;
+    };
+}
+
+#endif // STATE_HPP

ec7013dfbe4e340cf1cef234ecbd8e715e5d454f
Reim-developer <contact.kaxtr@gmail.com>
2025-03-28 22:51:02 +0700

    Update mainWindow.hpp to include context and signal management

--- src/mainWindow.hpp
+++ src/mainWindow.hpp
@@ -1,15 +1,17 @@
 #ifndef mainWindow_HPP
 #define mainWindow_HPP
 #include <QtWidgets/QMainWindow>
-#include "common/props.hpp"
+#include "common/include/context.hpp"
+#include "common/include/props.hpp"
 #include "utils/utils.hpp"
 #include "gui/include/label.hpp"
 #include "gui/include/dropbox.hpp"
 #include "gui/include/buttons.hpp"
+#include "common/include/signal.hpp"
 
 using namespace Reaction;
 using namespace Reaction::Utils;
-
+using namespace Reaction::Common;
 
 namespace Reaction {
     class MainWindow : public QMainWindow {
@@ -22,10 +24,15 @@ namespace Reaction {
     private:
         AppProps *props;
         Geometry *geometry;
+
+        Signal *signal;
+        Context *context;
+
         Gui::GuiLabel *guiLabel;
         Gui::GuiLabelUtils *guiLabelUtils;
         Gui::GuiDropbox *guiDropbox;
         Gui::GuiButtons *guiButtons;
+
     };
 }
 #endif // mainWindow_HPP

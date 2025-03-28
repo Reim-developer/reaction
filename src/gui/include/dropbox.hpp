f409e57b28a19f611b60cf160f42b0bac47a3fb1
Reim-developer <contact.kaxtr@gmail.com>
2025-03-28 21:22:23 +0700

    Define helper widget creator function 'setMultiComboBox'

--- src/gui/include/dropbox.hpp
+++ src/gui/include/dropbox.hpp
@@ -3,14 +3,21 @@
 #include <QtWidgets/QComboBox>
 #include <QtWidgets/QWidget>
 #include <QtCore/QString>
+#include <vector>
+
+using namespace std;
 
 namespace Reaction::Gui {
     class GuiDropbox {
         public:
-        QComboBox *setComboBox(
-            QWidget *widget, const QString &text,
-            const int x_loc, const int y_loc,
-            const int width, const int height
+        QComboBox *setComboBox(QWidget *widget, const QString &text,
+                               const int x_loc, const int y_loc,
+                               const int width, const int height
+        );
+        QComboBox *setMultiComboBox(QWidget *widget, const vector<QString> &text,
+                                    const int x_loc, const int y_loc,
+                                    const int width, const int height
+
         );
     };
 

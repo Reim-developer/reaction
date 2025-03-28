1f4495fbfd91166618e840663752d765dbf40497
Reim-developer <contact.kaxtr@gmail.com>
2025-03-29 04:58:31 +0700

    Refactor MainWindow to utilize InitUI for GUI setup and remove redundant components

--- src/gui/window.cc
+++ src/gui/window.cc
@@ -1,21 +1,17 @@
 #include "../mainWindow.hpp"
 #include "../common/include/props.hpp"
-#include "../common/include/signal.hpp"
 #include "../utils/utils.hpp"
 #include "QtCore/qobject.h"
-#include "QtWidgets/qcombobox.h"
-#include "QtWidgets/qpushbutton.h"
-#include "include/buttons.hpp"
-#include "include/dropbox.hpp"
 #include <QtWidgets/QWidget>
 #include <QtCore/QString>
-#include <vector>
+#include "init.hpp"
 #include <QtWidgets/QMainWindow>
 
 using namespace std;
 using namespace Reaction;
 using namespace Reaction::Utils;
 using namespace Reaction::Common;
+using namespace Reaction::Gui;
 
 MainWindow::MainWindow(QWidget *window) : QMainWindow(window) {
     props = new AppProps();
@@ -28,50 +24,21 @@ MainWindow::MainWindow(QWidget *window) : QMainWindow(window) {
 
     setWindowTitle(title_fmt);
     setFixedSize(800, 600);
-
     geometry = new Geometry();
     geometry->moveCenter(this);
-
-    guiLabel = new Gui::GuiLabel();
-    QLabel *deviceSelect = guiLabel->setLabel(this, "Device Selection:", 10, 30, 100, 30);
-    QLabel *bootFileSelect = guiLabel->setLabel(this, "Boot File Selection:", 10, 100, 100, 30);
-    QLabel *partitionSelect = guiLabel->setLabel(this, "Partition Selection", 10, 170, 100, 30);
-
-    guiLabelUtils = new Gui::GuiLabelUtils();
-    guiLabelUtils->setLabelFixedSize(deviceSelect, 12);
-    guiLabelUtils->setLabelFixedSize(bootFileSelect, 12);
-    guiLabelUtils->setLabelFixedSize(partitionSelect, 12);
-
-    guiDropbox = new Gui::GuiDropbox();
-    QComboBox *deviceList = guiDropbox->setComboBox(this, "No device found", 10, 60, 150, 30);
-    static_cast<Gui::GuiDropboxStyle*>(deviceList)->setTransparent(true);
-
-    QComboBox *bootSelctionList = guiDropbox->setComboBox(this, "No boot file", 10, 130, 150, 30);
-    static_cast<Gui::GuiDropboxStyle*>(bootSelctionList)->setTransparent(true);
-
-    vector<QString> partitionSelectionList = {"MBR", "GPT"};
-    QComboBox *selectPartition = guiDropbox->setMultiComboBox(this, partitionSelectionList, 10, 200,  150, 30);
-    static_cast<Gui::GuiDropboxStyle*>(selectPartition)->setTransparent(true);
-
-    guiButtons = new Gui::GuiButtons();
-
-    guiButtons->setButtons(this, "Device not found? Reload", 300, 60, 200, 30);
-    guiButtons->setButtons(this, "Choose device manually",530, 60, 200, 30);
-
-    QPushButton *openBootFileButton  = guiButtons->setButtons(this, "Choose boot file", 300, 130, 200, 30);
     
     signal = new Signal(this);
+    state = new State();
     context = new Context();
-    context->setOpenFileContext(openBootFileButton, signal, this, bootSelctionList);
+
+    InitUI *init = new InitUI();
+    init->setup(this, signal, state, context);
 }
 
 MainWindow::~MainWindow() {
     delete props;
     delete geometry;
-    delete guiLabel;
-    delete guiLabelUtils;
-    delete guiDropbox;
-    delete guiButtons;
     delete signal;
     delete context;
+    delete state;
 }

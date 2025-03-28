cd0a3a21759c3e0246c65369d5dfc53fd77d1a40
Reim-developer <contact.kaxtr@gmail.com>
2025-03-28 23:09:37 +0700

    Update Context::setOpenFileContext to include QMainWindow and QComboBox parameters

--- src/common/include/context.hpp
+++ src/common/include/context.hpp
@@ -1,12 +1,14 @@
 #ifndef CONTEXT_HPP
 #define CONTEXT_HPP
+#include "QtWidgets/qcombobox.h"
+#include "QtWidgets/qmainwindow.h"
 #include "QtWidgets/qpushbutton.h"
 #include "signal.hpp"
 
 namespace Reaction::Common {
     class Context {
         public:
-            void setOpenFileContext(QPushButton *button, Signal *signal);
+            void setOpenFileContext(QPushButton *button, Signal *signal, QMainWindow *windows, QComboBox *comboBox);
     };
 }
 

c48ed1fdfe9bae4fdd4a0234d90e4326729f7aac
Reim-developer <contact.kaxtr@gmail.com>
2025-03-28 23:09:18 +0700

    Enhance Context::setOpenFileContext to include file selection and update QComboBox with selected file name

--- src/common/context.cc
+++ src/common/context.cc
@@ -1,11 +1,28 @@
+#include "include/context.hpp"
+#include "QtCore/qdir.h"
+#include "QtCore/qfileinfo.h"
+#include "QtCore/qlogging.h"
+#include "QtWidgets/qcombobox.h"
+#include "QtWidgets/qfiledialog.h"
+#include "QtWidgets/qmainwindow.h"
 #include "QtWidgets/qpushbutton.h"
 #include "include/signal.hpp"
-#include "include/context.hpp"
+#include <QtWidgets/QFileDialog>
 
 using namespace Reaction::Common;
 
-void Context::setOpenFileContext(QPushButton *button, Signal *signal) {
-    QObject::connect(button, &QPushButton::clicked, signal, [=]() {
-        signal->openFileDialog();
-    });
+void Context::setOpenFileContext(QPushButton *button, Signal *signal,
+                                 QMainWindow *windows, QComboBox *comboBox) {
+  QObject::connect(button, &QPushButton::clicked, signal, [=]() {
+    QString filePath = QFileDialog::getOpenFileName(windows, "Choose boot file",
+                                                    QDir::homePath(),
+                                                    "Boot File (*.img *.iso)");
+
+            if (!filePath.isEmpty()) {
+                    QFileInfo fileInfo(filePath);
+                    QString fileName = fileInfo.fileName();
+                    comboBox->clear();
+                    comboBox->addItem(fileName);
+            }
+  });
 }

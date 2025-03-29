2fad701ba89410e129b98e1c5a377a94f9392949
Reim-developer <contact.kaxtr@gmail.com>
2025-03-30 06:26:21 +0700

    Adjust UI component sizes for improved layout and usability

--- src/gui/init.cc
+++ src/gui/init.cc
@@ -18,21 +18,21 @@ void InitUI::setup(MainWindow *window, Signal *signal, State *state, Context *co
     guiLabelUtils->setLabelFixedSize(partitionSelect, 12);
 
     GuiDropbox *guiDropbox = new GuiDropbox();
-    QComboBox *deviceList = guiDropbox->setComboBox(window, "No device found", 10, 60, 150, 30);
+    QComboBox *deviceList = guiDropbox->setComboBox(window, "No device found", 10, 60, 280, 30);
     static_cast<Gui::GuiDropboxStyle*>(deviceList)->setTransparent(true);
 
-    QComboBox *bootSelctionList = guiDropbox->setComboBox(window, "No boot file", 10, 130, 150, 30);
+    QComboBox *bootSelctionList = guiDropbox->setComboBox(window, "No boot file", 10, 130, 280, 30);
     static_cast<Gui::GuiDropboxStyle*>(bootSelctionList)->setTransparent(true);
 
     vector<QString> partitionSelectionList = {"MBR", "GPT"};
-    QComboBox *selectPartition = guiDropbox->setMultiComboBox(window, partitionSelectionList, 10, 200,  150, 30);
+    QComboBox *selectPartition = guiDropbox->setMultiComboBox(window, partitionSelectionList, 10, 200,  280, 30);
     static_cast<Gui::GuiDropboxStyle*>(selectPartition)->setTransparent(true);
 
     GuiButtons *guiButtons = new GuiButtons();
-    QPushButton *reloadDiskButton = guiButtons->setButtons(window, "Device not found? Reload", 300, 60, 200, 30);
-    QPushButton *openDeviceButton = guiButtons->setButtons(window, "Choose device manually",530, 60, 200, 30);
+    QPushButton *reloadDiskButton = guiButtons->setButtons(window, "Device not found? Reload", 360, 60, 200, 30);
+    QPushButton *openDeviceButton = guiButtons->setButtons(window, "Choose device manually",600, 60, 200, 30);
 
-    QPushButton *openBootFileButton  = guiButtons->setButtons(window, "Choose boot file", 300, 130, 200, 30);
+    QPushButton *openBootFileButton  = guiButtons->setButtons(window, "Choose boot file", 360, 130, 200, 30);
 
     context->setOpenFileContext(openBootFileButton, signal, window, bootSelctionList, state);
     context->setOpenDeviceContext(openDeviceButton, signal, window, deviceList, state);

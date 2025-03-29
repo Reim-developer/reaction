ba683ff75151c66e4eabb74b5e1235f3923bdaf2
Reim-developer <contact.kaxtr@gmail.com>
2025-03-30 05:39:22 +0700

    Add device path retrieval and update disk dialog to use State class

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -1,14 +1,18 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 #include "QtCore/qobject.h"
+#include "../common/include/state.hpp"
 #include "QtWidgets/qwidget.h"
 #include <QtWidgets/QWidget>
+
+using namespace Reaction::Common;
 
 namespace Reaction::Utils {
     class Utils {
         public:
             void moveCenter(QWidget *window);
-            QString showOpenDiskDialog(QWidget *window);
+            QString getDevicePath(const QString &device);
+            void showOpenDiskDialog(QWidget *window, State *state);
     };
 }
 

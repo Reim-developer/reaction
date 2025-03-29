2782b164323028c7c31e4a62f58e896ce80f0eab
Reim-developer <contact.kaxtr@gmail.com>
2025-03-29 20:00:56 +0700

    Add setOpenDeviceContext method to Context for device selection handling

--- src/common/context.cc
+++ src/common/context.cc
@@ -1,6 +1,8 @@
 #include "include/context.hpp"
 #include "QtCore/qdir.h"
 #include "QtCore/qfileinfo.h"
+#include "../utils/utils.hpp"
+#include "QtCore/qobject.h"
 #include "QtWidgets/qcombobox.h"
 #include "QtWidgets/qfiledialog.h"
 #include "QtWidgets/qmainwindow.h"
@@ -10,21 +12,37 @@
 #include <QtWidgets/QFileDialog>
 
 using namespace Reaction::Common;
+using namespace Reaction::Utils;
 
 void Context::setOpenFileContext(QPushButton *button, Signal *signal,
-                                 QMainWindow *windows, QComboBox *comboBox, State *state) {
+                                 QMainWindow *windows, QComboBox *comboBox,
+                                 State *state) {
   QObject::connect(button, &QPushButton::clicked, signal, [=]() {
     QString filePath = QFileDialog::getOpenFileName(windows, "Choose boot file",
                                                     QDir::homePath(),
                                                     "Boot File (*.img *.iso)");
 
-            if (!filePath.isEmpty()) {
-                    QFileInfo fileInfo(filePath);
-                    QString fileName = fileInfo.fileName();
-                    comboBox->clear();
-                    comboBox->addItem(fileName);
-                    
-                    state->isoFilePath = filePath;
-            }
+    if (!filePath.isEmpty()) {
+      QFileInfo fileInfo(filePath);
+      QString fileName = fileInfo.fileName();
+      comboBox->clear();
+      comboBox->addItem(fileName);
+
+      state->isoFilePath = filePath;
+    }
+  });
+}
+
+void Context::setOpenDeviceContext(QPushButton *button, Signal *signal,
+                                   QMainWindow *windows, QComboBox *comboBox,
+                                   State *state) {
+
+  QObject::connect(button, &QPushButton::clicked, signal, [=]() { 
+              class Utils *utils = new class Utils();
+              QString deviceName = utils->showOpenDiskDialog(windows);
+              if(!deviceName.isEmpty()) {
+                  comboBox->clear();
+                  comboBox->addItem(deviceName);
+              }
   });
 }

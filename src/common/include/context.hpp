9693ef7da1e2ddb4c4125b9c9500ee5fb016f1cc
Reim-developer <contact.kaxtr@gmail.com>
2025-03-30 06:21:49 +0700

    Add setReloadDiskContext method to Context class for improved disk management

--- src/common/include/context.hpp
+++ src/common/include/context.hpp
@@ -7,12 +7,17 @@
 #include "state.hpp"
 
 namespace Reaction::Common {
-    class Context {
-        public:
-            void setOpenFileContext(QPushButton *button, Signal *signal, QMainWindow *windows, QComboBox *comboBox, State *state);
-            void setOpenDeviceContext(QPushButton *button, Signal *signal, QMainWindow *windows, QComboBox *comboBox, State *state);
-    };
-}
-
+class Context {
+public:
+  void setOpenFileContext(QPushButton *button, Signal *signal,
+                          QMainWindow *windows, QComboBox *comboBox,
+                          State *state);
+  void setOpenDeviceContext(QPushButton *button, Signal *signal,
+                            QMainWindow *windows, QComboBox *comboBox,
+                            State *state);
+  void setReloadDiskContext(QPushButton *button, Signal *signal,
+                            QComboBox *comboBox, State *state);
+};
+} // namespace Reaction::Common
 
 #endif // CONTEXT_HPP

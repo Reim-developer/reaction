15f1f34fd9576dd13f58dc7c6d756f9c5d6b4606
Reim-developer <contact.kaxtr@gmail.com>
2025-03-30 19:11:30 +0700

    Add new context management functions for device auto-detection and current item retrieval

--- src/common/include/context.hpp
+++ src/common/include/context.hpp
@@ -7,17 +7,20 @@
 #include "state.hpp"
 
 namespace Reaction::Common {
-class Context {
-public:
-  void setOpenFileContext(QPushButton *button, Signal *signal,
-                          QMainWindow *windows, QComboBox *comboBox,
-                          State *state);
-  void setOpenDeviceContext(QPushButton *button, Signal *signal,
-                            QMainWindow *windows, QComboBox *comboBox,
-                            State *state);
-  void setReloadDiskContext(QPushButton *button, Signal *signal,
-                            QComboBox *comboBox, State *state);
-};
+      class Context {
+          public:
+            void setOpenFileContext(QPushButton *button, Signal *signal,
+                                    QMainWindow *windows, QComboBox *comboBox,
+                                    State *state);
+            void setOpenDeviceContext(QPushButton *button, Signal *signal,
+                                      QMainWindow *windows, QComboBox *comboBox,
+                                      State *state);
+            void setReloadDiskContext(QPushButton *button, Signal *signal,
+                                      QComboBox *comboBox, State *state);
+            void getCurrentItemContext(QComboBox *comboBox, Signal *singal, State *state);
+            void autoDetectDevice(QMainWindow *windows, QComboBox *comboBox, State *state);
+      };
+  
 } // namespace Reaction::Common
 
 #endif // CONTEXT_HPP

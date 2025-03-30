d486f4cb8363da6585d74c2118052050b69f34d5
Reim-developer <contact.kaxtr@gmail.com>
2025-03-31 06:29:05 +0700

    Define macro

---
+++ src/common/include/message.h
@@ -0,0 +1,13 @@
+#ifndef MESSAGE_H
+#define  MESSAGE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+#define DEVICE_NOT_FOUND "Device name or device path is missing. You can use reload button or choose device manually."
+#define DEVICE_PATH_NOT_FOUND "Device is not detected. Ensure the device is properly connected and recognized by the system"
+
+#ifdef __cplusplus
+}
+#endif
+#endif

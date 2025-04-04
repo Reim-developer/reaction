b9bdc6bce8dbf561cf0690f10ec5af54eeb4a822
Reim-developer <contact.kaxtr@gmail.com>
2025-04-05 05:28:00 +0700

    Define debug message

---
+++ src/cmd/include/macro.h
@@ -0,0 +1,7 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+#define INVALID_USAGE_MSG "Invalid usage. Try helper --help for more infomation\n"
+#define BAD_USAGE_MSG "Bad usage: %s\nUse helper --help for more infomation.\n"
+
+#endif // MACRO_H

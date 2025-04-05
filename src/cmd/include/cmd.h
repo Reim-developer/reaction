98917cd1c5401f397328e5fb044fc331cd37db60
Reim-developer <contact.kaxtr@gmail.com>
2025-04-06 05:15:54 +0700

    Migration all macros message debug to macro.h

--- src/cmd/include/cmd.h
+++ src/cmd/include/cmd.h
@@ -1,11 +1,6 @@
 #ifndef FORMAT_H
 #define FORMAT_H
 #include "../sys/include/status.h"
-#define MISSING_ARGS_MSG "Missing argument: device_path\nUsage: helper format <DEVICE_PATH>\n"
-#define OPEN_DEVICE_FAILED_MSG "Open device failed. Run with root permit and try again.\n"
-#define WRITE_DEVICE_FAILED_MSG "Write to device failed. Run with root permit and try again.\n"
-#define SYNC_DEVICE_FAILED_MSG "Sync device failed. Please try again.\n"
-#define FORMAT_SUCCESS_MSG "Format device successfully.\n"
 
 RESULT_STATUS format_cmd(int argv, char *args[]);
 

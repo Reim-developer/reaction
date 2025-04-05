8e7a41c53f04dcf727b2653d997785981ae98dfa
Reim-developer <contact.kaxtr@gmail.com>
2025-04-06 05:16:19 +0700

    Migration debug macro to macro.h

--- src/cmd/include/macro.h
+++ src/cmd/include/macro.h
@@ -3,5 +3,10 @@
 
 #define INVALID_USAGE_MSG "Invalid usage. Try helper --help for more infomation\n"
 #define BAD_USAGE_MSG "Bad usage: %s\nUse helper --help for more infomation.\n"
+#define MISSING_ARGS_MSG "Missing argument: device_path\nUsage: helper format <DEVICE_PATH>\n"
+#define OPEN_DEVICE_FAILED_MSG "Open device failed. Run with root permit and try again.\n"
+#define WRITE_DEVICE_FAILED_MSG "Write to device failed. Run with root permit and try again.\n"
+#define SYNC_DEVICE_FAILED_MSG "Sync device failed. Please try again.\n"
+#define FORMAT_SUCCESS_MSG "Format device successfully.\n"
 
 #endif // MACRO_H

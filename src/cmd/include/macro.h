54c49a6471ca740eec342d39e0beee1118c62ea7
Reim-developer <contact.kaxtr@gmail.com>
2025-04-06 11:13:31 +0700

    DEFINE new macro debug message

--- src/cmd/include/macro.h
+++ src/cmd/include/macro.h
@@ -1,12 +1,20 @@
 #ifndef MACRO_H
 #define MACRO_H
 
+#define NO_PERMIT_MSG "No permit operation. Are you run this command with root permit\n"
+
 #define INVALID_USAGE_MSG "Invalid usage. Try helper --help for more infomation\n"
 #define BAD_USAGE_MSG "Bad usage: %s\nUse helper --help for more infomation.\n"
-#define MISSING_ARGS_MSG "Missing argument: device_path\nUsage: helper format <DEVICE_PATH>\n"
+
+#define MISSING_ARGS_MSG "Missing argument: device_path\n"
 #define OPEN_DEVICE_FAILED_MSG "Open device failed. Run with root permit and try again.\n"
+
 #define WRITE_DEVICE_FAILED_MSG "Write to device failed. Run with root permit and try again.\n"
 #define SYNC_DEVICE_FAILED_MSG "Sync device failed. Please try again.\n"
+
 #define FORMAT_SUCCESS_MSG "Format device successfully.\n"
+#define DEVICE_NOT_FOUND_MSG "This device not found.\n"
+
+#define UMOUNT_DEVICE_FAILED_MSG "Unmount the device failed.\n"
 
 #endif // MACRO_H

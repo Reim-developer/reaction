492115eef12f629c8faa24fbb98f41da668e451b
Reim-developer <contact.kaxtr@gmail.com>
2025-04-07 11:37:02 +0700

    Fix typo in header file and add new debug message macro

--- src/cmd/include/macro.h
+++ src/cmd/include/macro.h
@@ -1,13 +1,13 @@
 #ifndef MACRO_H
 #define MACRO_H
 
-#define NO_PERMIT_MSG "No permit operation. Are you run this command with root permit\n"
+#define NO_PERMIT_MSG "No permit operation. Are you run this command with root permit?\n"
 
 #define INVALID_USAGE_MSG "Invalid usage. Try helper --help for more infomation\n"
 #define BAD_USAGE_MSG "Bad usage: %s\nUse helper --help for more infomation.\n"
 
 #define MISSING_ARGS_MSG "Missing argument: device_path\n"
-#define OPEN_DEVICE_FAILED_MSG "Open device failed. Run with root permit and try again.\n"
+#define OPEN_DEVICE_FAILED_MSG "Open device failed. Maybe device not found.\n"
 
 #define WRITE_DEVICE_FAILED_MSG "Write to device failed. Run with root permit and try again.\n"
 #define SYNC_DEVICE_FAILED_MSG "Sync device failed. Please try again.\n"
@@ -16,5 +16,8 @@
 #define DEVICE_NOT_FOUND_MSG "This device not found.\n"
 
 #define UMOUNT_DEVICE_FAILED_MSG "Unmount the device failed.\n"
+#define ADD_PARTITION_FAILED_MSG "Add partition for the disk failed.\n"
+
+#define COMMIT_PARTITION_FAILED_MSG "Commit partition for the disk failed.\n"
 
 #endif // MACRO_H

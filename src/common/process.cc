7e8ce4862fea59e570361656ced0fc74e592ea17
Reim-developer <contact.kaxtr@gmail.com>
2025-04-09 18:42:52 +0700

    Implement process spawning with error handling in spawnProcess method

--- src/common/process.cc
+++ src/common/process.cc
@@ -1,14 +1,29 @@
 #include "include/process.hpp"
-#include "QtCore/qdebug.h"
+#include "../cmd/sys/include/status.h"
 #include "QtCore/qlogging.h"
+#include "include/message.hpp"
 #include <QtCore/QProcess>
+#include <QtCore/QFile>
+#include <QtWidgets/QMessageBox>
+#include <QtWidgets/QWidget>
 
 using namespace Reaction::Common;
 
-void Process::setProcess(const QString &programPath, const QStringList &params) {
-    QProcess process;
-    process.start(programPath, params);
+int Process::spawnProcess(QWidget *windows, const QString &programPath, const QStringList &params) { 
+    QFile fileProgram = QFile(programPath);
 
-    QString output = QString::fromUtf8(process.readAllStandardOutput());
-    qDebug() << "Output: " << output;
+    if(!fileProgram.exists()) {
+        QMessageBox::information(windows, "Information", CLI_PROGRAM_PATH_NOT_FOUND);
+        return CLI_PATH_NOT_FOUND;
+    }
+
+    QProcess process;
+    
+    process.start("pkexec", params);
+    process.waitForFinished();
+
+    QString stderr_o = QString::fromUtf8(process.readAllStandardError());
+    qDebug() << stderr_o;
+
+    return SUCCESS;
 }
